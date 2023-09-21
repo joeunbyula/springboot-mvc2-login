9d88ea6c21670ff3afae51b0421696bf7f462a2f
joeunbyul <gkdldy1109@gmail.com>
2023-09-22 00:43:23 +0900

    interceptor / argumentResolver

--- src/main/java/hello/login/web/filter/LoginCheckFilter.java
+++ src/main/java/hello/login/web/filter/LoginCheckFilter.java
@@ -26,7 +26,7 @@ public class LoginCheckFilter implements Filter {
             log.info("인증 체크 필터 시작 {}", requestURI);
             if(isLoginCheckPath(requestURI)) {
                 log.info("인증 체크 로직 실행 {}", requestURI);
-                HttpSession session = httpRequest.getSession();
+                HttpSession session = httpRequest.getSession(false);
                 if(session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) ==  null) {
                     log.info("미인증 사용자 요청 {}", requestURI);
                     httpResponse.sendRedirect("/login?redirectURL="+requestURI);
